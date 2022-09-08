d0fa37d6f2886b7dbf5d0c8e78d84e2be1c9ebf7
JadKHaddad <jadkhaddad@gmail.com>
2022-09-08 11:49:08 +0200

    User logic

--- rocust_lib/src/main.rs
+++ rocust_lib/src/main.rs
@@ -1,6 +1,7 @@
 use parking_lot::RwLock;
 use rand::Rng;
 use reqwest::Client;
+use reqwest::RequestBuilder;
 use std::collections::HashMap;
 use std::fmt;
 use std::sync::{Arc, Mutex};
@@ -77,7 +78,7 @@ impl fmt::Display for EndPointResults {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "Total Response Time [{}] | Total Requests [{}] | Average Response [{}]",
+            "Total Requests [{}] | Total Response Time [{}] | Average Response [{}]",
             self.total_requests, self.total_response_time, self.average_response
         )
     }
@@ -88,14 +89,16 @@ pub struct EndPoint {
     method: Method,
     url: String,
     results: Arc<RwLock<EndPointResults>>,
+    headers: Option<HashMap<String, String>>,
 }
 
 impl EndPoint {
-    pub fn new(method: Method, url: String) -> EndPoint {
+    pub fn new(method: Method, url: String, headers: Option<HashMap<String, String>>) -> EndPoint {
         EndPoint {
             method,
             url,
             results: Arc::new(RwLock::new(EndPointResults::new())),
+            headers,
         }
     }
 
@@ -129,17 +132,110 @@ pub struct TestData {
     //TODO
 }
 
+#[derive(Clone, Debug)]
+pub struct User {
+    client: Client,
+    id: String,
+    sleep: u64,
+    host: Arc<String>,
+    global_endpoints: Arc<Vec<EndPoint>>,
+    global_headers: Option<HashMap<String, String>>,
+    global_results: Arc<RwLock<EndPointResults>>,
+    results: Arc<RwLock<EndPointResults>>,
+    endpoints: Arc<Vec<EndPoint>>, //TODO  make this a hashmap with the endpoint name as the key and the endpoint result as the value
+}
+
+impl fmt::Display for User {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "User [{}] | Results [{}] | Endpoints [{:?}]",
+            self.id, self.results.read(), self.endpoints
+        )
+    }
+}
+
+impl User {
+    pub fn new(
+        id: String,
+        sleep: u64,
+        host: Arc<String>,
+        global_endpoints: Arc<Vec<EndPoint>>,
+        global_headers: Option<HashMap<String, String>>,
+        global_results: Arc<RwLock<EndPointResults>>,
+    ) -> User {
+        User {
+            client: Client::new(),
+            id,
+            sleep,
+            host,
+            global_endpoints,
+            global_headers,
+            global_results,
+            results: Arc::new(RwLock::new(EndPointResults::new())),
+            endpoints: Arc::new(Vec::new()),
+        }
+    }
+
+    fn add_headers(&self, mut request: RequestBuilder, endpoint: &EndPoint) -> RequestBuilder {
+        if let Some(global_headers) = &self.global_headers {
+            for (key, value) in global_headers {
+                request = request.header(key, value);
+            }
+        }
+        if let Some(headers) = &endpoint.headers {
+            for (key, value) in headers {
+                request = request.header(key, value);
+            }
+        }
+        request
+    }
+
+    async fn perform(&self) {
+        loop {
+            let endpoint = Test::select_random_endpoint(&self.global_endpoints);
+            let url = format!("{}{}", self.host, endpoint.get_url());
+            let mut request = match endpoint.get_method() {
+                Method::GET => self.client.get(&url),
+                Method::POST => self.client.post(&url),
+                Method::PUT => self.client.put(&url),
+                Method::DELETE => self.client.delete(&url),
+            };
+            request = self.add_headers(request, endpoint);
+            let start = Instant::now();
+            //TODO ConnectionErrors are not handled here yet
+            if let Ok(response) = request.send().await {
+                let duration = start.elapsed();
+                println!(
+                    "user: {} | {} {} | {:?} | thread id: {:?}",
+                    self.id,
+                    response.status(),
+                    url,
+                    duration,
+                    thread::current().id()
+                );
+                endpoint.add_response_time(duration.as_millis() as u32);
+                self.
+                    add_response_time(duration.as_millis() as u32);
+            }
+            tokio::time::sleep(Duration::from_secs(Test::select_random_sleep(self.sleep))).await;
+        }
+    }
+}
+
 //this struct is used to control the test from another thread
 #[derive(Clone, Debug)]
 pub struct TestHandler {
     token: Arc<Mutex<CancellationToken>>,
+    results: Arc<RwLock<EndPointResults>>,
 }
 
 impl TestHandler {
-    pub fn new(token: Arc<Mutex<CancellationToken>>) -> TestHandler {
-        TestHandler {
-            token,
-        }
+    pub fn new(
+        token: Arc<Mutex<CancellationToken>>,
+        results: Arc<RwLock<EndPointResults>>,
+    ) -> TestHandler {
+        TestHandler { token, results }
     }
 
     pub fn stop(&self) {
@@ -150,6 +246,29 @@ impl TestHandler {
     //TODO
 }
 
+trait Updatble {
+    fn add_response_time(&self, response_time: u32);
+}
+
+impl Updatble for Test {
+    fn add_response_time(&self, response_time: u32) {
+        self.results.write().add_response_time(response_time);
+    }
+}
+
+impl Updatble for User {
+    fn add_response_time(&self, response_time: u32) {
+        self.global_results.write().add_response_time(response_time);
+        self.results.write().add_response_time(response_time);
+    }
+}
+
+impl Updatble for TestHandler {
+    fn add_response_time(&self, response_time: u32) {
+        self.results.write().add_response_time(response_time);
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Test {
     status: Status,
@@ -157,8 +276,8 @@ pub struct Test {
     users: u32,
     run_time: Option<u64>,
     sleep: u64,
-    host: String,
-    end_points: Vec<EndPoint>,
+    host: Arc<String>,
+    endpoints: Arc<Vec<EndPoint>>,
     global_headers: Option<HashMap<String, String>>,
     results: Arc<RwLock<EndPointResults>>,
     start_timestamp: Option<Instant>,
@@ -171,7 +290,7 @@ impl Test {
         run_time: Option<u64>,
         sleep: u64,
         host: String,
-        end_points: Vec<EndPoint>,
+        endpoints: Vec<EndPoint>,
         global_headers: Option<HashMap<String, String>>,
     ) -> Self {
         Self {
@@ -180,8 +299,8 @@ impl Test {
             users,
             run_time,
             sleep,
-            host,
-            end_points,
+            host: Arc::new(host),
+            endpoints: Arc::new(endpoints),
             global_headers,
             results: Arc::new(RwLock::new(EndPointResults::new())),
             start_timestamp: None,
@@ -189,25 +308,30 @@ impl Test {
         }
     }
 
-    pub fn create_handler(&self) -> TestHandler {
-        TestHandler::new(self.token.clone())
+    pub fn creat_user(&self, id: String) -> User {
+        User::new(
+            id,
+            self.sleep,
+            self.host.clone(),
+            self.endpoints.clone(),
+            self.global_headers.clone(),
+            self.results.clone(),
+        )
     }
 
-    fn add_response_time(&self, response_time: u32) {
-        self.results.write().add_response_time(response_time);
+    pub fn create_handler(&self) -> TestHandler {
+        TestHandler::new(self.token.clone(), self.results.clone())
     }
 
     pub async fn select_run_mode_and_run(&mut self) -> Result<(), Elapsed> {
         self.start_timestamp = Some(Instant::now());
         return match self.run_time {
-            Some(run_time) => {
-                self.run_with_timeout(run_time).await
-            }
+            Some(run_time) => self.run_with_timeout(run_time).await,
             None => {
                 self.run_forever().await;
                 Ok(())
             }
-        }
+        };
     }
 
     pub async fn run(mut self) -> Self {
@@ -238,44 +362,10 @@ impl Test {
         for i in 0..self.users {
             let user_id = i + 1;
             println!("spawning user: {}", user_id);
-            let test = self.clone();
+            let user = self.creat_user(user_id.to_string());
+
             let join_handle = tokio::spawn(async move {
-                let client = Client::new();
-                loop {
-                    let end_point = test.select_random_end_point();
-                    let url = format!("{}{}", test.host, end_point.get_url());
-
-                    let mut request = match end_point.get_method() {
-                        Method::GET => client.get(&url),
-                        Method::POST => client.post(&url),
-                        Method::PUT => client.put(&url),
-                        Method::DELETE => client.delete(&url),
-                    };
-
-                    if let Some(global_headers) = &test.global_headers {
-                        for (key, value) in global_headers {
-                            request = request.header(key, value);
-                        }
-                    }
-
-                    let start = Instant::now();
-                    //TODO ConnectionErrors are not handled here yet
-                    if let Ok(response) = request.send().await {
-                        let duration = start.elapsed();
-                        println!(
-                            "user: {} | {} {} | {:?} | thread id: {:?}",
-                            user_id,
-                            response.status(),
-                            url,
-                            duration,
-                            thread::current().id()
-                        );
-                        end_point.add_response_time(duration.as_millis() as u32);
-                        test.add_response_time(duration.as_millis() as u32);
-                    }
-                    tokio::time::sleep(Duration::from_secs(test.select_random_sleep()))
-                        .await;
-                }
+                user.perform().await;
             });
             join_handles.push(join_handle);
         }
@@ -290,15 +380,15 @@ impl Test {
         }
     }
 
-    pub fn select_random_end_point(&self) -> &EndPoint {
+    pub fn select_random_endpoint(endpoints: &Arc<Vec<EndPoint>>) -> &EndPoint {
         let mut rng = rand::thread_rng();
-        let index = rng.gen_range(0..self.end_points.len());
-        &self.end_points[index]
+        let index = rng.gen_range(0..endpoints.len());
+        &endpoints[index]
     }
 
-    pub fn select_random_sleep(&self) -> u64 {
+    pub fn select_random_sleep(sleep: u64) -> u64 {
         let mut rng = rand::thread_rng();
-        rng.gen_range(0..self.sleep)
+        rng.gen_range(0..sleep)
     }
 
     pub fn stop(&self) {
@@ -324,7 +414,7 @@ impl fmt::Display for Test {
             self.run_time.unwrap_or(0),
             self.sleep,
             self.host,
-            self.end_points,
+            self.endpoints,
             self.global_headers.as_ref().unwrap_or(&HashMap::new()),
             self.results.read(),
             self.start_timestamp,
@@ -342,10 +432,10 @@ async fn main() {
         5,
         "https://httpbin.org".to_string(),
         vec![
-            EndPoint::new(Method::GET, "/get".to_string()),
-            EndPoint::new(Method::POST, "/post".to_string()),
-            EndPoint::new(Method::PUT, "/put".to_string()),
-            EndPoint::new(Method::DELETE, "/delete".to_string()),
+            EndPoint::new(Method::GET, "/get".to_string(), None),
+            EndPoint::new(Method::POST, "/post".to_string(), None),
+            EndPoint::new(Method::PUT, "/put".to_string(), None),
+            EndPoint::new(Method::DELETE, "/delete".to_string(), None),
         ],
         None,
     );
