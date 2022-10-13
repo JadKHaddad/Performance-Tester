1591d3b07bc541bccd4efd112bed784e737ecc8d
JadKHaddad <jadkhaddad@gmail.com>
2022-10-13 10:09:08 +0200

    Jsonable Trait

--- rocust_lib/src/traits.rs
+++ rocust_lib/src/traits.rs
@@ -1,7 +1,9 @@
 use crate::{Results, Status};
 use async_trait::async_trait;
 use parking_lot::RwLock;
-use std::{sync::Arc, time::Duration};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{error::Error, sync::Arc, time::Duration};
+use tokio::fs;
 
 pub trait HasResults {
     fn add_response_time(&self, response_time: u32);
@@ -20,4 +22,32 @@ pub trait Runnable {
     fn finish(&self);
     fn get_status(&self) -> Status;
     fn get_id(&self) -> &String;
+}
+
+#[async_trait]
+pub trait Jsonable // TODO: use somewhere
+where
+    Self: Sized + DeserializeOwned + Serialize
+{
+    fn from_json(json: &str) -> Result<Self, Box<dyn Error>> {
+        let res: Self = serde_json::from_str(json)?;
+        Ok(res)
+    }
+
+    async fn from_file(path: &str) -> Result<Self, Box<dyn Error>> {
+        let json = fs::read_to_string(path).await?;
+        let res: Self = Jsonable::from_json(&json)?;
+        Ok(res)
+    }
+
+    fn into_json(&self) -> Result<String, Box<dyn Error>>{
+        let json = serde_json::to_string(self)?;
+        Ok(json)
+    }
+    
+    async fn into_file(&self, path: &str) -> Result<(), Box<dyn Error>>{
+        let json = self.into_json()?;
+        fs::write(path, json).await?;
+        Ok(())
+    }
 }
